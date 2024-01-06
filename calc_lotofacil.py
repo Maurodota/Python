b9a37d4c206d3b261b76c37c8207cf6261f8167b
Dota <maurodota@hotmail.com>
2024-01-06 13:33:21 -0300

    simpleCod

--- calc_lotofacil.py
+++ calc_lotofacil.py
@@ -1,101 +1,25 @@
+import random
+from collections import Counter
 
-import random 
-import nampy
-num1 = 0
-num2 = 0
-num3 = 0
-num4 = 0
-num5 = 0
-num6 = 0
-num7 = 0
-num8 = 0
-num9 = 0
-num10 = 0
-num11 = 0
-num12 = 0
-num13 = 0
-num14 = 0
-num15 = 0
-num16 = 0
-num17 = 0
-num18 = 0
-num19 = 0
-num20 = 0
-num21 = 0
-num22 = 0
-num23 = 0
-num24 = 0
-num25 = 0
+numeros_por_jogo = 15
+quantidade_numeros = 25
 
 apostas = int(input('Digite a quantidade de apostas?: '))
 
-for aposta in range(apostas):
-    lotofacil = random.randint(1,25) 
-    
-    if lotofacil == 1:
-        num1 += 1
-    elif lotofacil == 2:
-        num2 += 1
-    elif lotofacil == 3:
-         num3 += 1
-    elif lotofacil == 4:
-         num4 += 1
-    elif lotofacil == 5:
-         num5 += 1
-    elif lotofacil == 6:
-         num6 += 1
-    elif lotofacil == 7:
-         num7 += 1
-    elif lotofacil == 8:
-         num8 += 1
-    elif lotofacil == 9:
-         num9 += 1
-    elif lotofacil == 10:
-         num10 += 1
-    elif lotofacil == 11:
-         num11 += 1
-    elif lotofacil == 12:
-         num12 += 1
-    elif lotofacil == 13:
-         num13 += 1
-    elif lotofacil == 14:
-         num14 += 1
-    elif lotofacil == 15:
-         num15 += 1
-    elif lotofacil == 16:
-         num16 += 1
-    elif lotofacil == 17:
-         num17 += 1
-    elif lotofacil == 18:
-         num18 += 1
-    elif lotofacil == 19:
-         num19 += 1
-    elif lotofacil == 20:
-         num20 += 1
-    elif lotofacil == 21:
-         num21 += 1
-    elif lotofacil == 22:
-         num22 += 1
-    elif lotofacil == 23:
-         num23 += 1
-    elif lotofacil == 24:
-         num24 += 1
-    elif lotofacil == 25:
-         num25 += 1
-    
+# Inicializar a lista de contadores para cada número
+contadores = Counter()
 
-print ('Numero de vezes que esse numero apareceu: 1',num1)
-print ('Numero de vezes que esse numero apareceu: 2',num2)
-print ('Numero de vezes que esse numero apareceu: 3',num3)
-print ('Numero de vezes que esse numero apareceu: 4',num4)
-print ('Numero de vezes que esse numero apareceu: 5',num5)
-print ('Numero de vezes que esse numero apareceu: 6',num6)
-print ('Numero de vezes que esse numero apareceu: 7',num7)
-print ('Numero de vezes que esse numero apareceu: 8',num8)
-print ('Numero de vezes que esse numero apareceu: 9',num9)
-print ('Numero de vezes que esse numero apareceu: 10',num10)
-print ('Numero de vezes que esse numero apareceu: 11',num11)
-print ('Numero de vezes que esse numero apareceu: 12',num12)
-print ('Numero de vezes que esse numero apareceu: 13',num13)
-print ('Numero de vezes que esse numero apareceu: 14',num14)
-print ('Numero de vezes que esse numero apareceu: 15',num15)
+for aposta in range(apostas):
+    lotofacil = random.sample(range(1, quantidade_numeros + 1), numeros_por_jogo)
+    
+    # Atualizar os contadores para cada número sorteado
+    contadores.update(lotofacil)
+
+# Obter os 15 números mais sorteados
+mais_sorteados = contadores.most_common(15)
+
+# Imprimir os resultados formatados
+print('\nOs 15 números mais sorteados foram:')
+for i, (numero, quantidade) in enumerate(mais_sorteados, 1):
+    print(f'{i}. Número {numero}: {quantidade} vezes')
+
